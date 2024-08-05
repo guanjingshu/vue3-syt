cf3db932a626364342cfa77208d20d6957b6e22b
guanjingshu <97731963+guanjingshu@users.noreply.github.com>
2024-08-05 23:28:46 +0800

    feat: 头部组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>
-    <h1>App</h1>
+  <div class="container">
+    <HospitalTop />
+
+    <div class="content">
+      <!-- 展示路由组件的区域 -->
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 <script setup lang="ts"></script>
-<style lang="scss"></style>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .content {
+    margin-top: 70px;
+    width: 1200px;
+    min-height: 700px;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "@/style/reset.scss";
 
+import HospitalTop from "@/components/hospital_top/index.vue";
 const app = createApp(App);
-
+app.component("HospitalTop", HospitalTop);
 app.mount("#app");
