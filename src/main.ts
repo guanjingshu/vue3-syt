a7bdae1f7df5b018a5b0437685d3e020bef29ecf
guanjingshu <97731963+guanjingshu@users.noreply.github.com>
2024-08-07 22:36:26 +0800

    feat: 增加轮播图+搜索框

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import App from './App.vue';
 import '@/style/reset.scss';
 
@@ -9,4 +11,5 @@ const app = createApp(App);
 app.component('HospitalTop', HospitalTop);
 app.component('HosipitalBottom', HosipitalBottom);
 app.use(router);
+app.use(ElementPlus);
 app.mount('#app');
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>我是首页</div>
+  <div>
+    <!-- 首页轮播图的结构 -->
+    <Carousel />
+    <!-- 首页搜索医院的表单区域 -->
+    <Search />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  //引入首页轮播图组件
+  import Carousel from './carousel/index.vue';
+  //引入首页搜索的组件
+  import Search from './search/index.vue';
+</script>
 
 <style scoped lang="scss"></style>
