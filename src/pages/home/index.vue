8b598c1502fe4a749e1cddedd3036f45b682bd62
guanjingshu <97731963+guanjingshu@users.noreply.github.com>
2024-08-08 23:02:17 +0800

    feat: 添加科室和平台公告页面

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,6 +4,25 @@
     <Carousel />
     <!-- 首页搜索医院的表单区域 -->
     <Search />
+
+    <!-- 底部展示医院的结构 -->
+    <el-row gutter="20">
+      <el-col :span="20">
+        <!-- 等级子组件 -->
+        <Level @getLevel="getLevel" />
+        <!--地区 -->
+        <Region @getRegion="getRegion" />
+
+        <!-- 展示医院的结构 -->
+        <!-- <div class="hospital" v-if="hasHospitalArr.length > 0">
+          <Card class="item" v-for="(item, index) in hasHospitalArr" :key="index" :hospitalInfo="item" />
+        </div>
+        <el-empty v-else description="暂无数据" /> -->
+      </el-col>
+      <el-col :span="4">
+        <Tip />
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -12,6 +31,14 @@
   import Carousel from './carousel/index.vue';
   //引入首页搜索的组件
   import Search from './search/index.vue';
+  // 引入首页等级的组件
+  import Level from './level/index.vue';
+  // 引入首页地区选择的组件
+  import Region from './region/index.vue';
+  //展示医院新的的卡片组件
+  import Card from './card/index.vue';
+  //引入右侧组件
+  import Tip from './tip/index.vue';
 </script>
 
 <style scoped lang="scss"></style>
